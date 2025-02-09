70940e38b90eee6a7a3b3cdabad7c1d7ae51ba53
bbulls14 <bbulls14@gmail.com>
2025-02-09 00:05:09 -0500

    quarto updates

--- nvim/.config/nvim/lua/plugins/nvim-web-devicons.lua
+++ nvim/.config/nvim/lua/plugins/nvim-web-devicons.lua
@@ -5,25 +5,7 @@ return {
 		config = function()
 			--empty table, icons configured in init.lua
 			require("nvim-web-devicons").setup({
-				-- Globally enable default icons (set to false to disable)
-				  --override_by_filename = {
-				  
-				    --[".toml"] = {
-					--    icon = "🅣", -- Stylized text search icon
-					--    color = "#9c4221", -- Match color from the image
-					--    cterm_color = "124", -- Terminal color from the image
-					--    name = "Toml", -- Name for the icon
-					--  },
-					  --fix the color
-				    --[".ipynb"] = {
-					--    icon = "🌐", -- Stylized text search icon
-					--    color = "#FFA500", -- Match color from the image
-					--    cterm = "124",
-					--    name = "Ipynb", -- Name for the icon
-					--  },
 
-				  --},
-				  --default = true,
 			})
 		end,
 	},
